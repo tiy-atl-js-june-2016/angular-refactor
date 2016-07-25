cc37c01962810d26209dcdde53d195a1ddd7f7ff
Tim Whitacre <tim@timw.co>
2016-07-25 10:40:39 -0400

    wrote out our service methods

--- src/js/services/gif.service.js
+++ src/js/services/gif.service.js
@@ -1,10 +1,30 @@
 function GifService ($http, SERVER) {
 
   this.createGif = createGif;
+  this.readGif   = readGif;
+  this.readGifs  = readGifs;
+  this.updateGif = updateGif;
+  this.deleteGif = deleteGif;
 
 
   function createGif (gif) {
     return $http.post(SERVER.URL, gif);
+  }
+
+  function readGif (id) {
+    return $http.get(SERVER.URL + id);
+  }
+
+  function readGifs () {
+    return $http.get(SERVER.URL);
+  }
+
+  function updateGif (gif) {
+    return $http.put(SERVER.URL + gif._id, gif);
+  }
+
+  function deleteGif (id) {
+    return $http.delete(SERVER.URL + id);
   }
 
 }
